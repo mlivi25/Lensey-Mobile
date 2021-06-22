53e17f379576da8858b20808a5f79ce13efb2b7a
Legz4Dayz <mitchlivingston25@gmail.com>
2021-06-21 22:19:17 -0500

    Added TODO list to main

--- lensey_mobile/lib/main.dart
+++ lensey_mobile/lib/main.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:lensey_mobile/Presentation/sign-in.dart';
 
 void main() {
   runApp(MyApp());
+
+  //TODO: Create DIO singleton
+  //TODO: Setup Routing
+  //TODO: Configure Get-it
+  //TODO: Add Multi-Provider
+  //TODO: Add Multi-Bloc Provider
 }
 
 class MyApp extends StatelessWidget {
@@ -13,53 +20,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      home: SignInPage(title: 'Flutter Demo Home Page'),
     );
   }
 }
